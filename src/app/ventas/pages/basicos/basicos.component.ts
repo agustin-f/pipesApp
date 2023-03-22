f68313ed14859667b2ca1ec56733f4adbc69b2ae
Agustin Farisano <farisanoagustin@gmail.com>
2023-03-22 15:22:04 -0300

    angular date pipes examples

--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+  fecha:Date = new Date();
+
   nombreLower:string = 'maría'
   nombreUpper:string = 'MARÍA'
   nombreCompleto:string = 'MaRíA FerNanDez'
