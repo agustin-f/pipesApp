e8000ae0e34b38584623c4bef355ce7b71a2f118
Agustin Farisano <farisanoagustin@gmail.com>
2023-03-22 15:33:12 -0300

    change locale lenguage for use in date pipes examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,6 +8,12 @@ import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
 import { VentasModule } from './ventas/ventas.module';
+
+//Cambiar el locale de la app
+import localES from '@angular/common/locales/es-AR' 
+import {registerLocaleData} from '@angular/common'
+registerLocaleData(localES);
+
 
 @NgModule({
   declarations: [
@@ -20,7 +26,9 @@ import { VentasModule } from './ventas/ventas.module';
     AppRouterModule,
     VentasModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'es-AR'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
