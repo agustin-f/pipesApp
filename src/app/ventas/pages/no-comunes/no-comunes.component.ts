6d6fc221ac2500866f51b0f24d0626abbcf04918
Agustin Farisano <farisanoagustin@gmail.com>
2023-03-27 15:36:29 -0300

    i18nPlural pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -7,12 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoComunesComponent implements OnInit {
 
+  //i18nSelect pipe
   nombre:string = 'Juan'
   genero:string = 'masculino'
 
   invitacionMapa = {
     'masculino': 'invitarlo',
     'femenino': ' invitarla'
+  }
+
+  //i18nPlural pipe
+  clientes:string[] = ['María','Juan','Pedro','Susana', 'Carlos'];
+
+  clientesMapa = {
+    '=0': 'No tenemos nigun cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos dos clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
   }
 
   constructor() { }
